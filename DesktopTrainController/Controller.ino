5b1dfaaf52bb6bf7752f5aa2d2dddf9dde07307d
Zhang Muhua <31773545+TSDArthur@users.noreply.github.com>
2018-06-12 23:36:12 +0800

    6.12
    6.12

--- DesktopTrainController/Controller.ino
+++ DesktopTrainController/Controller.ino
@@ -17,21 +17,21 @@
 	===========================================
 	--Train:
 	0.SPEED -> INT
-	1.REVERSER -> BOOL
+	1.REVERSER -> INT
 	2.POWER -> U8
 	3.BRAKE -> U8
 	4.SIGNAL -> INT
 	5.SIGNAL_DISTANCE -> INT
 	6.SPEED_LIMIT -> INT
 	7.HORN -> BOOL
-	8.SPEED_CONST -> BOOL
+	8.SPEED_CONST -> INT
 	9.MASTER_KEY -> BOOL
 	===========================================
 	Never mind scandal and liber!
 */
 
 #include "DueTimer.h"
-
+//
 #define SWITCH_C 0
 #define SWITCH_F 1
 #define ENCODER 2
@@ -51,7 +51,10 @@
 #define SIGNAL_DISTANCE_N2 1500
 #define SIGNAL_DISTANCE_N3 1000
 #define SIGNAL_DISTANCE_N4 500
+#define SIGNAL_DISTANCE_DE 0
 #define SIGNAL_PASS 0
+#define HORN_OFF 0
+#define HORN_ON 1
 #define SPEED_LIMIT_MIN 0
 #define SPEED_LIMIT_DEF 30
 #define SPEED_LIMIT_MAX 400
@@ -60,9 +63,37 @@
 #define SPEED_CONST_MAX 400
 #define MASTER_KEY_OFF 0
 #define MASTER_KEY_ON 1
-//
+//Devices
 #define DEVICE_NUMBER 8
 #define DEVICE_TYPE_NUMBER 3
+#define ACTIVE 0
+#define NO_ACTIVE 1
+#define NO_READY 0
+#define READY 1
+//Train
+#define TRAIN_DATA_NUMBER 10
+#define _INT 0
+#define _BOOL 1
+#define SPEED 0
+#define REVERSER 1
+#define POWER 2
+#define BRAKE 3
+#define SIGNAL 4
+#define SIGNAL_DISTANCE 5
+#define SPEED_LIMIT 6
+#define HORN 7
+#define SPEED_CONST 8
+#define MASTER_KEY 9
+//HMI
+#define HMI_SCRIPT_NUM 11
+#define HMI_END_SYM 10
+//PC
+#define FILTER '|'
+#define START_SYM '#'
+#define END_SYM '!'
+#define RECIEVE_DELAY 10
+#define SEND_DELAY 10
+#define TIMER_TICK 100
 //
 typedef void (*funcPoint)();
 //
@@ -92,16 +123,22 @@ void interrupt6();
 void interrupt7();
 
 //change device type here
-int deviceType[DEVICE_NUMBER] = {SWITCH_F, SWITCH_F, SWITCH_C, SWITCH_C, SWITCH_C, SWITCH_C, SWITCH_C, SWITCH_C};
+const int deviceType[DEVICE_NUMBER] = {SWITCH_F, SWITCH_F, SWITCH_C, SWITCH_C, SWITCH_C, SWITCH_C, SWITCH_C, SWITCH_C};
 //change functions here
-funcPoint Interrputs[DEVICE_NUMBER] = {interrupt0, interrupt1, interrupt2, interrupt3, interrupt4, interrupt5, interrupt6, interrupt7};
+const funcPoint Interrputs[DEVICE_NUMBER] = {interrupt0, interrupt1, interrupt2, interrupt3, interrupt4, interrupt5, interrupt6, interrupt7};
 //all use PULL_UP gpio mode
-int devicePins[DEVICE_NUMBER];
+const int devicePins[DEVICE_NUMBER] = {30, 31, 32, 33, 34, 35, 36, 37};
+//train data id
+const int dataDefault[TRAIN_DATA_NUMBER] = {SPEED_MIN, REVERSER_NEUTRAL, POWER_MIN, BRAKE_MIN, SIGNAL_RED, SIGNAL_DISTANCE_DE, SPEED_LIMIT_DEF, HORN_OFF, SPEED_CONST_MIN, MASTER_KEY_OFF};
+const int dataType[TRAIN_DATA_NUMBER] = {_INT, _INT, _INT, _INT, _INT, _INT, _INT, _BOOL, _INT, _BOOL};
+//communication
+const String HMIScript[HMI_SCRIPT_NUM] = { "spd.val=", "reserver.val=", "pwr.val=", "brake.val=", "sig.val=", "sigdis.val=", "spdlim.val=", "horn.val=", "speedconst.val=", "mstkey.val=", "0xFF"};
+const int HMIMap[TRAIN_DATA_NUMBER] = {SPEED, REVERSER, POWER, BRAKE, SIGNAL, SIGNAL_DISTANCE, SPEED_LIMIT, HORN, SPEED_CONST, MASTER_KEY};
 
-class ControllerDevices
+class DevicesManager
 {
 public:
-	void Initialize(int devicePins[], int deviceType[], funcPoint Interrupts[])
+	Initialize(int devicePins[], const int deviceType[], const funcPoint Interrupts[])
 	{
 		//define gpio mode
 		for (int i = 0; i < DEVICE_NUMBER; i++)
@@ -111,8 +148,53 @@ public:
 			attachInterrupt(devicePins[i], Interrupts[i], deviceType[i] == SWITCH_F ? FALLING : CHANGE);
 	}
 	//get state
-	int GetDeviceState(int deviceID, int devicePins[])
+	int GetDeviceState(int deviceID, const int devicePins[])
 	{
-		return digitalRead(devicePins[deviceID]);
+		return digitalRead(devicePins[deviceID]) ? ACTIVE : NO_ACTIVE;
+	}
+	//check state
+	int IsReady(const int devicePins[])
+	{
+		for (int i = 0; i < DEVICE_NUMBER; i++)
+			if (GetDeviceState(i, devicePins) == ACTIVE)return NO_READY;
+		return READY;
+	}
+};
+
+class TrainManager
+{
+public:
+	int trainData[TRAIN_DATA_NUMBER];
+	Initialize(const int dataDefault[])
+	{
+		//set default data to Train Manager
+		for (int i = 0; i < TRAIN_DATA_NUMBER; i++)
+			trainData[i] = dataDefault[i];
+	}
+	//!!for INT use SetData
+	//!!for BOOL use SetReversal
+	void SetData(int dataID, int value)
+	{
+		trainData[dataID] = value;
+	}
+
+	void SetReversal(int dataID)
+	{
+		trainData[dataID] = !trainData[dataID];
+	}
+	//get train data
+	int GetData(int dataID)
+	{
+		return trainData[dataID];
+	}
+};
+
+class CommunicationManager
+{
+public:
+	Initialize()
+	{
+		Serial.begin(115200);
+		Serial1.begin(115200);
 	}
 }
